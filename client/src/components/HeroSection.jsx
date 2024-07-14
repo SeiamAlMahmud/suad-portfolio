1bc9ac3f98389593dc6e9819854e2301265c4bd1
SeiamAlMahmud <samahmud49@gmail.com>
2024-07-14 13:44:57 +0000

    hero section done

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
-import Typewriter from 'typewriter-effect';
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import imag from '../assets/sunday.png'
 const HeroSection = () => {
-    const text =[]
+  const words = [' Suad', ' a graphic designer']
+  const [text] = useTypewriter({
+    words: words,
+    loop: 0
+  })
   return (
-<>
-<div>
-<Typewriter
-  options={{
-    strings: [' I am Hello', ' I am World'],
-    autoStart: true,
-    loop: true,
-  }}
-/>
-</div>
-</>  
-)
+    <>
+      <div>
+        <div className="flex justify-between items-center    flex-col sm:flex-row px-4 bg-[#000] pb-16 pt-10">
+          <div className="flex-1 flex items-center text-2xl text-white">
+            <h1 style={{ margin: '50px' }} className='sm:text-5xl'>
+              I'm
+              <span style={{ fontWeight: "bold", color: 'green' }}>
+                {text}
+              </span>
+              <span className='text-red-700'>
+              <Cursor />
+              </span>
+            </h1>
+
+          </div>
+          <div className="flex-shrink-0 flex items-center rounded-[50%] bg-[#0000005d] overflow-hidden  border-teal-400 border-4 shadow-lg shadow-teal-400 md:mr-20">
+            <img src={imag} alt="Description" className="w-52 h-52 rounded-full sm:h-72 sm:w-72 md:w-96 md:h-96" /> {/* Adjust size as needed */}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default HeroSection
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -32,12 +32,12 @@ return () => window.removeEventListener('scroll', handleScroll);
     // console.log(lastScrollY,scrollingDown)
     return (
         <>
-            <div className=" flex w-full items-center justify-between px-2 py-2 bg-[#fff] border-b-2 border-r-blue-300 ">
+            <div className=" flex w-full items-center justify-between px-2 py-2 bg-gray-950 border-b-2 border-r-blue-300 ">
                 <h1 className='text-4xl  md:text-6xl mr-2 font-bold text-[#09da8d]'>SUAD</h1>
-                <ul className=' gap-3 py-4 text-md hidden  sm:flex sm:gap-7 text-xl md:text-2xl font-semibold'>
-                    <li className='px-2 py-1  hover:bg-teal-200 duration-150 ease-in rounded-2xl'><Link to={"/"}>About me</Link></li>
-                    <li className='px-2 py-1  hover:bg-teal-200 duration-150 ease-in rounded-2xl'><Link to={"/"}>Contact us</Link></li>
-                    <li className='px-2 py-1  hover:bg-teal-200 duration-150 ease-in rounded-2xl'><Link to={"/"}> About me</Link></li>
+                <ul className=' gap-3 py-4 text-md hidden  sm:flex sm:gap-7 text-xl md:text-2xl font-semibold text-white'>
+                    <li className='px-2 py-1  hover:bg-teal-200 duration-150 ease-in rounded-2xl hover:text-black'><Link to={"/"}>About me</Link></li>
+                    <li className='px-2 py-1  hover:bg-teal-200 duration-150 ease-in rounded-2xl hover:text-black'><Link to={"/"}>Contact us</Link></li>
+                    <li className='px-2 py-1  hover:bg-teal-200 duration-150 ease-in rounded-2xl hover:text-black'><Link to={"/"}> About me</Link></li>
                 </ul>
                 <div className='sm:hidden'>
                     <AnchorTemporaryDrawer></AnchorTemporaryDrawer>
